566842875c5dc17a89a30574b693cdab7f937776
Samuel Monroe <spat.monroe@gmail.com>
2016-03-13 14:34:02 +0100

    Add feature spec to cover login errors

---
+++ spec/features/user/user_bad_login_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.feature 'user logging in', '
+  As a registered user
+  In order to user Learnento service
+  I want to log in my account' do
+
+  scenario 'Registered user wants to log in Pravatar' do
+    i_am_on_root_page
+    i_go_on_login_form
+    i_fill_the_form_in
+    i_log_in
+
+    my_session_is_active
+  end
+
+  private
+
+  let!(:user){User.create(email: "foo@bar.com", password: "password")}
+  def i_am_on_root_page
+    visit '/'
+  end
+
+  def i_go_on_login_form
+    root.access_login
+  end
+
+  def i_fill_the_form_in
+    login.fill_login_form(user.email, 'passwoordz')
+  end
+
+  def i_log_in
+    login.confirm_login_form
+  end
+
+  def my_session_is_active
+    expect(page).to have_selector('[data-purpose="login-error"]')
+  end
+
+end
