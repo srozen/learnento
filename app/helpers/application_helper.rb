4c27f8c8f0d58d948ad306264026077ee724b848
Samuel Monroe <spat.monroe@gmail.com>
2016-03-19 16:44:46 +0100

    Adds a success messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_success!(message)
+    html = <<-HTML
+    <div id="error_explanation" data-purpose="devise-alert" class="alert alert-success" role="alert">
+      <ul>#{message}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
 end
