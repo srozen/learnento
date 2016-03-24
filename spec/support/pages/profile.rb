5214c3cbaa4dc6ef4fb339914d8f75a0c16d4502
Samuel Monroe <spat.monroe@gmail.com>
2016-03-24 16:56:23 +0100

    Auto-generated files

--- spec/support/pages/profile.rb
+++ spec/support/pages/profile.rb
@@ -2,9 +2,14 @@ module Spec
   module Pages
     class Profile < SitePrism::Page
       element :edit_profile_button, '[data-purpose="edit-profile-button"]'
+      element :send_friend_request_button, '[data-purpose="send-friend-request-button"]'
 
       def access_profile_edition
         edit_profile_button.click
+      end
+
+      def send_friend_request
+        send_friend_request_button.click
       end
     end
   end
