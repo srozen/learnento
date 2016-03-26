4b4d74745fa9c251b1a64749bd16772d90e42822
Samuel Monroe <spat.monroe@gmail.com>
2016-03-26 15:22:37 +0100

    Seeded database to fastly reset it and make live tests on friendship system.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+                {email: 'alice@gmail.com', password: 'password'},
+                {email: 'bob@gmail.com', password: 'password'},
+                {email: 'charlie@gmail.com', password: 'password'}
+            ])
