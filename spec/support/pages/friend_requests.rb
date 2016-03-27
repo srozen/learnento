4dcdcc8dd36c570dee804cc73cbea284af0dc752
Samuel Monroe <spat.monroe@gmail.com>
2016-03-27 20:31:31 +0200

    Feature spec, cancelling a friend request made by the user

--- spec/support/pages/friend_requests.rb
+++ spec/support/pages/friend_requests.rb
@@ -3,6 +3,7 @@ module Spec
     class FriendRequests < SitePrism::Page
       element :accept_friend_request_button, '[data-purpose="accept-friend-request-button"]'
       element :decline_friend_request_button, '[data-purpose="decline-friend-request-button"]'
+      element :cancel_friend_request_button, '[data-purpose="cancel-friend-request-button"]'
 
       def accept_friend_request
         accept_friend_request_button.click
@@ -10,6 +11,10 @@ module Spec
 
       def decline_friend_request
         decline_friend_request_button.click
+      end
+
+      def cancel_friend_request
+        cancel_friend_request_button.click
       end
     end
   end
