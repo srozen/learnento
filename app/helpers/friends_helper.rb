8e426361cfc8d6c6fe71f18370573f6d07b317dd
Samuel Monroe <spat.monroe@gmail.com>
2016-03-29 23:20:14 +0200

    User is prompt a modal to ensure he wants to delete a friend. Using a partial render.

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,5 +1,9 @@
 module FriendsHelper
   def remove_friend_button(friend)
-    button_to 'Delete Friend', friend_path(friend), method: 'delete', class: 'btn btn-danger', 'data-purpose': 'remove-friend-button'
+    button_to 'Delete Friend', friend_path(friend), method: 'delete', class: 'btn btn-danger', 'data-purpose': 'confirm-remove-friend-button'
+  end
+
+  def remove_friend_modal(friend)
+    render 'friends/delete_modal', friend: friend
   end
 end
