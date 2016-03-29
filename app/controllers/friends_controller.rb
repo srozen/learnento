b4a4d5cd929e96a6bc27e46d254df5cc59a704d3
Samuel Monroe <spat.monroe@gmail.com>
2016-03-29 22:45:07 +0200

    User can delete friend from his friendlist.

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -2,4 +2,10 @@ class FriendsController < ApplicationController
   def index
     @friends = current_user.friends
   end
+
+  def destroy
+    friend = User.find(params[:id])
+    current_user.remove_friend(friend)
+    redirect_to friends_path
+  end
 end
--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,2 +1,5 @@
 module FriendsHelper
+  def remove_friend_button(friend)
+    button_to 'Delete Friend', friend_path(friend), method: 'delete', class: 'btn btn-danger', 'data-purpose': 'remove-friend-button'
+  end
 end
