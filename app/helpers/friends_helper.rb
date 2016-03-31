cc345129c048a1949269fcb2d010dfd23a9d070e
Samuel Monroe <spat.monroe@gmail.com>
2016-03-31 20:17:48 +0200

    Blocking and unblocking friends is working

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -3,7 +3,23 @@ module FriendsHelper
     button_to 'Delete Friend', friend_path(friend), method: 'delete', class: 'btn btn-danger', 'data-purpose': 'confirm-remove-friend-button'
   end
 
+  def block_friend_button(friend)
+    button_to 'Block Friend', friend_path(friend, block: 'true'), method: 'put', class: 'btn btn-warning', 'data-purpose': 'confirm-block-friend-button'
+  end
+
+  def unblock_friend_button(friend)
+    button_to 'Unblock Friend', friend_path(friend, block: 'false'), method: 'put', class: 'btn btn-xs btn-success', 'data-purpose': 'unblock-friend-button'
+  end
+
   def remove_friend_modal(friend)
     render 'friends/delete_modal', friend: friend
+  end
+
+  def block_friend_modal(friend)
+    render 'friends/block_modal', friend: friend
+  end
+
+  def blocked_friends_collapse(blocked_friends)
+    render 'friends/blocked_collapse', blocked_friends: blocked_friends
   end
 end
