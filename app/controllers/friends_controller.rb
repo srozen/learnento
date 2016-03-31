f59668bb50c1715b8b59c30331aacad0f22d0ef7
Samuel Monroe <spat.monroe@gmail.com>
2016-03-31 20:15:27 +0200

    Update methods allow blocking/unblocking

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,17 @@
 class FriendsController < ApplicationController
   def index
     @friends = current_user.friends
+    @blocked_friends = current_user.blocked_friends
+  end
+
+  def update
+    friend = User.find(params[:id])
+    if params[:block] == 'true'
+      current_user.block_friend(friend)
+    else
+      current_user.unblock_friend(friend)
+    end
+    redirect_to friends_path
   end
 
   def destroy
