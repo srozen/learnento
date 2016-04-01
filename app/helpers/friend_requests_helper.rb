345ae0d233b0c1fca7dd4191474300d7de7c2485
Samuel Monroe <spat.monroe@gmail.com>
2016-04-01 09:25:37 +0200

    Display a pending state or the request modal

--- app/helpers/friend_requests_helper.rb
+++ app/helpers/friend_requests_helper.rb
@@ -1,9 +1,9 @@
 module FriendRequestsHelper
-  def friend_request_button
-    if !current_user.pending_friends.where(id: @user.id).empty?
+  def friend_request_button(user)
+    if !current_user.pending_friends.where(id: user.id).empty?
       button_tag 'Pending Request', class: 'btn btn-primary disabled', 'data-purpose': 'pending-friend-request-button'
-    elsif current_user != @user && !current_user.friends_with?(@user)
-      link_to 'Friend Request', {:controller => "friend_requests", :action => "create", :id => @user.id}, class: 'btn btn-primary', 'data-purpose': 'send-friend-request-button', :method => "post"
+    elsif current_user != user && !current_user.friends_with?(user)
+      render 'users/request_modal', user: user
     end
   end
 
