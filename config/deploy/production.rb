8bcf0f15bcbcc907dd21c00b2a9bbd0f329a76a7
Samuel Monroe <spat.monroe@gmail.com>
2016-04-06 22:44:54 +0200

    Forgot the role db for the deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '46.101.155.249', user: 'deploy', roles: %w{web app}
+server '46.101.155.249', user: 'deploy', roles: %w{web app db}
 
 #application information
 set :branch,    "master"
