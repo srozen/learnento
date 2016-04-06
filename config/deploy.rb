9b1e1261831b850fac6ca25574f1f7c3b4e37747
Samuel Monroe <spat.monroe@gmail.com>
2016-04-06 23:38:20 +0200

    Add a task for seeding production database

--- config/deploy.rb
+++ config/deploy.rb
@@ -88,9 +88,19 @@ namespace :deploy do
     ENV['UNTIL'] ||= 10.minutes.from_now.to_datetime.to_formatted_s(:rfc822)
   end
 
+  desc 'Runs rake db:seed'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
 
 end
-
 
 
 
