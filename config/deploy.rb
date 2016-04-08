dee3ea12bd67b4e15a72e9ea68642fa946a84a53
Samuel Monroe <spat.monroe@gmail.com>
2016-04-08 11:52:39 +0200

    db:reset task in deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -98,6 +98,16 @@ namespace :deploy do
       end
     end
   end
+  desc 'Runs rake db:reset'
+  task :reset => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:reset"
+        end
+      end
+    end
+  end
 
 
 end
