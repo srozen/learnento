8139a74d28b7a0b0dde8f16e235e75ca97491be4
Samuel Monroe <spat.monroe@gmail.com>
2016-04-09 16:01:45 +0200

    Type-ahead search engine

--- app/assets/javascripts/controllers/user-search-controller.js
+++ app/assets/javascripts/controllers/user-search-controller.js
@@ -1,9 +1,30 @@
 angular.module('Learnento').controller('UserSearchController', ['$scope', 'User', function($scope, User){
-    $scope.users = []
-    $scope.search = function(searchTerm) {
-        var params = {'keywords': searchTerm}
+    var page = 0;
+
+    User.all().success(function(data){
+        $scope.users = data;
+    });
+
+    $scope.search = function(searchTerm, page) {
+        var params = {'keywords': searchTerm, 'page': page};
         User.all(params).success(function(data){
             $scope.users = data;
         });
-    }
+    };
+
+    $scope.userLink = function(id) {
+        return '/users/'+id;
+    };
+
+    $scope.previousPage = function(){
+        if (page > 0) {
+            page--;
+            $scope.search($scope.keywords, page);
+        }
+    };
+
+    $scope.nextPage = function(){
+        page++;
+        $scope.search($scope.keywords, page);
+    };
 }]);
