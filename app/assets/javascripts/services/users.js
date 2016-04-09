ac7e0c5dbc255e4b8bae167ad9860c063179155c
Samuel Monroe <spat.monroe@gmail.com>
2016-04-09 16:11:49 +0200

    Must encapsulate provider for production

--- app/assets/javascripts/services/users.js
+++ app/assets/javascripts/services/users.js
@@ -1,7 +1,7 @@
-angular.module('Learnento').factory('User', function UserFactory($http){
+angular.module('Learnento').factory('User', ['$http', function UserFactory($http){
    return {
         all: function(params){
             return $http({method: 'GET', url: '/users.json', params: params})
         }
     }
-});
+}]);
