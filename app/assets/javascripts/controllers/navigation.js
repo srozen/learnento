ce09d42014a0f3ea326356ad7546124497335b3a
Samuel Monroe <spat.monroe@gmail.com>
2016-04-20 11:16:20 +0200

    Readd  initialization

--- app/assets/javascripts/controllers/navigation.js
+++ app/assets/javascripts/controllers/navigation.js
@@ -1,5 +1,6 @@
 angular.module('Learnento').controller('NavigationController', ['Authentication', '$scope', '$rootScope', function(Authentication, $scope, $rootScope){
     $scope.loggedIn = Authentication.loggedIn();
+    $scope.currentUser = Authentication.currentUser();
     $scope.logout = function(){
         Authentication.logout();
     };
