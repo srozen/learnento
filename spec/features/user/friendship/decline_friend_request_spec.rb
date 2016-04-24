e40c886b367095bc3f7da3511444209fe20b2ef0
Samuel Monroe <spat.monroe@gmail.com>
2016-04-24 15:48:51 +0200

    Small sleep time is necessary to perform visual changes in the page

--- spec/features/user/friendship/decline_friend_request_spec.rb
+++ spec/features/user/friendship/decline_friend_request_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature 'Declining a friend request', '
       i_check_my_friend_requests_menu
       i_have_a_new_friend_request
       i_decline_the_friend_request
-
+      sleep 0.5
       the_request_disappeared
     end
   end
