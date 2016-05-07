c94127e09a7fee017cd3497a96f64bfc7af8b053
Samuel Monroe <spat.monroe@gmail.com>
2016-05-07 17:24:58 +0200

    Message creation triggers an emitting via the node/socket server

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -6,6 +6,13 @@ class Api::V1::MessagesController < ApiController
     current_user = authenticate_token!
     friend = User.find(req['data']['attributes']['recipientId'])
     create_message!(current_user, friend, req['data']['attributes']['message'])
+    friend.increment!(:active_messaging_notifications, by= 1)
+
+    data = {
+        id: friend.id,
+        message: req['data']['attributes']['message']
+    }
+    $redis.publish "messaging#{friend.id}", data.to_json
 
     render json: ''
   end
--- nodeserver/server.js
+++ nodeserver/server.js
@@ -6,13 +6,15 @@ var clients = {};
 io.on('connection', function(socket){
     console.log('Client ', socket.id, ' logged in.');
     var subFriendships = require('redis').createClient();
+    var subMessaging = require('redis').createClient();
 
     socket.on('storeUserId', function(data){
         clients[data.id] = socket.id;
         clients[socket.id] = data.id;
-        subFriendships.subscribe('notify' + data.id)
+        subFriendships.subscribe('notify' + data.id);
+        subMessaging.subscribe('messaging' + data.id);
         console.log(clients)
-    })
+    });
 
     socket.on('disconnect', function(){
         subFriendships.unsubscribe('notify' + clients[socket.id]);
@@ -23,8 +25,14 @@ io.on('connection', function(socket){
     });
 
     subFriendships.on('message', function(channel, message){
-        data = JSON.parse(message)
-        socket.emit('friendRequest', JSON.parse(message))
+        data = JSON.parse(message);
+        socket.emit('friendRequest', JSON.parse(message));
+        console.log(message)
+    });
+
+    subMessaging.on('message', function(channel, message){
+        data = JSON.parse(message);
+        socket.emit('messaging', JSON.parse(message));
         console.log(message)
     })
 
